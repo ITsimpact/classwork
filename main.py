abdf7ccef9264d74e8a71bf74c7d600e749f77f2
Aziz <sobirjonov0909@gmail.com>
2024-08-07 20:41:06 +0500

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1
+# import math
+# a = 3
+# b = 4
+# c = math.sqrt (a**2 + b**2)
+# print(f"{c} dlina gipotenuzi")
+#2
+# import math
+# radius = 7
+# perimeter = 2 * math.pi * radius
+# area = math.pi * radius ** 2
+# print(f"Perimetr: {perimeter}, Plowad: {area}")
+#3
+# import math
+# x = math.pi / 4
+# f_x = math.sin(x) + math.cos(x)
+# print(f_x)
